63d7011e425b63c76a174beba049a84f1c194327
joshwrn <joshnwarren@gmail.com>
2021-05-30 15:13:01 -0700

    about section

--- src/components/About.js
+++ src/components/About.js
@@ -4,10 +4,18 @@ import '../styles/about.css';
 const About = () => {
   return (
     <div id="about">
-      <div id="shop-gradient">
-        <h1 id="shop-title">About</h1>
+      <div id="about-gradient">
+        <div id="about-details">
+          <h1 id="about-title">ABOUT US</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eros
+            lacus, molestie elementum sollicitudin non, efficitur sit amet
+            ipsum. Fusce commodo hendrerit odio, sodales rutrum sem bibendum
+            sed. Duis dapibus nulla et enim convallis maximus.
+          </p>
+        </div>
       </div>
-      <img id="hero-about" src="/img/about.jpg" alt="hero" />
+      <img id="about-hero" src="/img/about.jpg" alt="hero" />
     </div>
   );
 };
