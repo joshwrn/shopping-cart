8a49060efa569d6e46d293247ebb116c0dcac01b
joshwrn <joshnwarren@gmail.com>
2021-05-31 16:08:03 -0700

    browser router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import BrowserRouter from 'react-router-dom/BrowserRouter';
 import './styles/styles.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
@@ -26,47 +27,49 @@ function App() {
   ]);
 
   return (
-    <Router>
-      <div className="App">
-        <Nav cart={cart} setCart={setCart} />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/women">
-            <Women
-              cards={cards}
-              categories={categories}
-              setCategories={setCategories}
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Router>
+        <div className="App">
+          <Nav cart={cart} setCart={setCart} />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/women">
+              <Women
+                cards={cards}
+                categories={categories}
+                setCategories={setCategories}
+              />
+            </Route>
+            <Route exact path="/men">
+              <Men
+                cards={cards}
+                categories={categories}
+                setCategories={setCategories}
+              />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/checkout">
+              <Checkout cart={cart} setCart={setCart} />
+            </Route>
+            <Route
+              exact
+              path="/shop/item/:title"
+              render={(props) => (
+                // eslint-disable-next-line react/jsx-props-no-spreading
+                <Item {...props} cards={cards} cart={cart} setCart={setCart} />
+              )}
             />
-          </Route>
-          <Route exact path="/men">
-            <Men
-              cards={cards}
-              categories={categories}
-              setCategories={setCategories}
-            />
-          </Route>
-          <Route exact path="/about">
-            <About />
-          </Route>
-          <Route exact path="/checkout">
-            <Checkout cart={cart} setCart={setCart} />
-          </Route>
-          <Route
-            exact
-            path="/shop/item/:title"
-            render={(props) => (
-              // eslint-disable-next-line react/jsx-props-no-spreading
-              <Item {...props} cards={cards} cart={cart} setCart={setCart} />
-            )}
-          />
-          <Route exact path="/search">
-            <Search cards={cards} />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+            <Route exact path="/search">
+              <Search cards={cards} />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </BrowserRouter>
   );
 }
 
